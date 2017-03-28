ad6e71f410a03923525fce5e7a07102ea3dd9445
Kevin Leung <kevinresol@gmail.com>
2017-03-28 16:14:06 +0800

    Fix haxe 3.2.1

--- src/tink/stringly/CommaSeparatedArray.hx
+++ src/tink/stringly/CommaSeparatedArray.hx
@@ -4,17 +4,17 @@ package tink.stringly;
 abstract CommaSeparatedArray<T>(Array<T>) from Array<T> to Array<T> {
 	@:from
 	public static function fromStringToString(v:String):CommaSeparatedArray<String>
-		return [for(item in v.split(',')) (item:tink.Stringly)]; // TODO: support quoted strings if they contains a comma?
+		return [for(item in v.split(',')) ((item:tink.Stringly):String)]; // TODO: support quoted strings if they contains a comma?
 		
 	@:from
 	public static function fromStringToInt(v:String):CommaSeparatedArray<Int>
-		return [for(item in v.split(',')) (item:tink.Stringly)];
+		return [for(item in v.split(',')) ((item:tink.Stringly):Int)];
 		
 	@:from
 	public static function fromStringToBool(v:String):CommaSeparatedArray<Bool>
-		return [for(item in v.split(',')) (item:tink.Stringly)];
+		return [for(item in v.split(',')) ((item:tink.Stringly):Bool)];
 		
 	@:from
 	public static function fromStringToFloat(v:String):CommaSeparatedArray<Float>
-		return [for(item in v.split(',')) (item:tink.Stringly)];
+		return [for(item in v.split(',')) ((item:tink.Stringly):Float)];
 }
